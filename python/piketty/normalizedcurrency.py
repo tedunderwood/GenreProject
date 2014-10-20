4c575eac3e9eb2de3f5a01ada01e3be452151038
tedunderwood <tunder@illinois.edu>
2014-10-20 15:25:02 -0500

    Updated code to generate wordcounts for stats grads.

--- python/piketty/normalizedcurrency.py
+++ python/piketty/normalizedcurrency.py
@@ -1,11 +1,12 @@
 import sys, os, csv
 import SonicScrewdriver as utils
 
-metafile = '/Users/tunder/Dropbox/GenreProject/metadata/unifiedficsample.csv'
+metafile = '/Users/tunder/Dropbox/GenreProject/metadata/ficsampleimprovedmetadata.csv'
 
 id2date = dict()
 date2wordcount = dict()
 dateset = set()
+id2wordcount = dict()
 
 with open(metafile, encoding = 'utf-8') as f:
     reader = csv.reader(f)
@@ -16,24 +17,37 @@ with open(metafile, encoding = 'utf-8') as f:
         code = row[0]
         date = int(row[1])
         dateset.add(date)
-        wordcount = int(row[2])
+        wordcount = int(row[3])
 
         id2date[code] = date
         utils.addtodict(date, wordcount, date2wordcount)
+        id2wordcount[code] = wordcount
+
 
 date2moneycount = dict()
+idnword2moneycount = dict()
 
 datafile = '/Volumes/TARDIS/work/moneycontext/fifteenwords.tsv'
 
 with open(datafile, encoding = 'utf-8') as f:
     for line in f:
         fields = line.split('\t')
+        code = fields[0]
         date = int(fields[1])
         if date not in dateset:
             print("Missing " + str(date))
+        keyword = fields[2]
         category = fields[3]
         if category == "money":
             utils.addtodict(date, 1, date2moneycount)
+            if code in idnword2moneycount:
+                if keyword in idnword2moneycount[code]:
+                    idnword2moneycount[code][keyword] += 1
+                else:
+                    idnword2moneycount[code][keyword] = 1
+            else:
+                idnword2moneycount[code] = dict()
+                idnword2moneycount[code][keyword] = 1
 
 datelist = list(dateset)
 datelist.sort()
@@ -52,6 +66,17 @@ with open(outfile, mode='w', encoding = 'utf-8') as f:
         strratio = ('%.6f' % ratio)
         row = [strdate, money, total, strratio]
         writer.writerow(row)
+
+outfile = '/Users/tunder/Dropbox/GenreProject/python/piketty/filtered_moneywords_by_vol.csv'
+
+with open(outfile, mode = 'w', encoding = 'utf-8') as f:
+    writer = csv.writer(f)
+    row = ['idcode', 'word', 'count']
+    writer.writerow(row)
+    for code, subdict in idnword2moneycount.items():
+        for word, count in subdict.items():
+            row = [code, word, str(count)]
+            writer.writerow(row)
 
 
 
