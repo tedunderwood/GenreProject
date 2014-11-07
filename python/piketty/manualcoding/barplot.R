af3ee269d7eb655c36f44956a6e7d595ff3c787a
tedunderwood <tunder@illinois.edu>
2014-11-07 12:39:26 -0600

    Data.

--- python/piketty/manualcoding/barplot.R
+++ python/piketty/manualcoding/barplot.R
@@ -1,9 +1,10 @@
 money = read.csv('/Users/tunder/Dropbox/GenreProject/python/piketty/manualcoding/normalbyvol.csv', stringsAsFactors = F)
 money$logval <- as.numeric(money$logval)
+money$normval <- as.numeric(money$normval)
 themeans = numeric()
 for (floor in seq(1750, 1900, 50)) {
   ceiling = floor + 50
-  newmean = mean(money$logval[money$date >= floor & money$date < ceiling])
+  newmean = median(money$normval[money$date >= floor & money$date < ceiling])
   themeans = c(themeans, newmean)
 }
-barplot(themeans, main = "Hoyt/Ted's 300 volumes, mean log(moneyreference / wage)", names.arg = c('1750-99', '1800-49', '1850-99', '1900-49'))
+barplot(themeans, main = "Ted & Hoyt's 300 volumes, median(moneyreference / wage)", names.arg = c('1750-99', '1800-49', '1850-99', '1900-49'))
