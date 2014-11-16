ddfa3bad9635647bb27e29a6c664e328c5faab5c
tedunderwood <tunder@illinois.edu>
2014-11-16 10:41:30 -0600

    Version of barplot I actually used.

--- python/piketty/manualcoding/barplot.R
+++ python/piketty/manualcoding/barplot.R
@@ -3,9 +3,9 @@ money$logval <- as.numeric(money$logval)
 money$normval <- as.numeric(money$normval)
 money$nominalval <- as.numeric(money$nominalval)
 themeans = numeric()
-for (floor in seq(1750, 1925, 25)) {
-  ceiling = floor + 25
-  newmean = median(money$nominalval[money$date >= floor & money$date < ceiling])
+for (floor in seq(1750, 1925, 50)) {
+  ceiling = floor + 50
+  newmean = median(money$normval[money$date >= floor & money$date < ceiling])
   themeans = c(themeans, newmean)
 }
 # barplot(themeans, main = "Rich & Ted's 424 snippets, median (reference / wage)", ylab = "nominal value in £", names.arg = c('1750', '60', '70', '80', '90', '1800', '10', '20', '30', '40', '50', '60', '70', '80', '90', '1900', '10', '20', '30', '40'))
@@ -13,7 +13,7 @@ for (floor in seq(1750, 1925, 25)) {
 # ('1750', '1760', '1770', '1780', '1790', '1800', '1810', '1820', '1830', '1840', '1850', '1860', '1870', '1880', '1890', '1900', '1910', '1920', '1930', '1940')
 library(ggplot2)
 library(scales)
-db <- data.frame(x = as.factor(c('1750', '1775', '1800', '1825', '1850', '1875', '1900', '1925')), y = themeans)
-q <- ggplot(db, aes(x=x, y = y)) + geom_bar(stat = 'identity', fill = 'cornflowerblue') + xlab(' ') + scale_y_continuous("") + 
-  theme(text = element_text(size = 18)) + ggtitle("Median percentage (value/wage), 719 snippets & vols\n")
+db <- data.frame(x = as.factor(c('1750-1799', '1800-1849', '1850-1899', '1900-1949')), y = themeans)
+q <- ggplot(db, aes(x=x, y = y)) + geom_bar(stat = 'identity', fill = 'cornflowerblue') + xlab(' ') + scale_y_continuous("",labels = percent) + 
+  theme(text = element_text(size = 20)) + ggtitle("Median amounts of money mentioned in fiction,\n as a percentage of the average annual wage\n")
 plot(q)
